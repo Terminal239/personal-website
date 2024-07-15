0486f79699ed33d5349e7ffd8991648ba1bddf72
Terminal239 <affan.shaikh2309@gmail.com>
2024-07-15 16:45:47 +0530

    Added social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,27 @@
 import Container from "./Reusable/Container";
+import { GithubFilledIcon, LeetcodeIcon, LinkedInIcon } from "./Reusable/Icons";
 
 type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer>
-      <Container> </Container>
+    <footer className="text-gray-700">
+      <Container className="flex items-center justify-between py-4">
+        <div className="flex items-center justify-between gap-6">
+          <p>&copy; 2024 Designed & Developed by Affan</p>
+        </div>
+        <div className="socials flex items-center gap-4">
+          <a href="https://github.com/Terminal239">
+            <GithubFilledIcon height={24} width={24} />
+          </a>
+          <a href="https://www.linkedin.com/in/affan-shaikh-0ab391268">
+            <LinkedInIcon height={24} width={24} />
+          </a>
+          <a href="https://leetcode.com/u/Terminal239/">
+            <LeetcodeIcon height={24} width={24} />
+          </a>
+        </div>
+      </Container>
     </footer>
   );
 };
--- components/Reusable/Icons.tsx
+++ components/Reusable/Icons.tsx
@@ -11,6 +11,43 @@ const SiteIcon = (props: React.SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
+const LinkedInIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width={props.width ?? 24} height={props.height ?? 24} viewBox="0 0 24 24" color={"currentColor"} fill={"currentColor"} xmlns="http://www.w3.org/2000/svg">
+    <path d="M6.5 8C7.32843 8 8 7.32843 8 6.5C8 5.67157 7.32843 5 6.5 5C5.67157 5 5 5.67157 5 6.5C5 7.32843 5.67157 8 6.5 8Z" color={"currentColor"} fill={"currentColor"} />
+    <path d="M5 10C5 9.44772 5.44772 9 6 9H7C7.55228 9 8 9.44771 8 10V18C8 18.5523 7.55228 19 7 19H6C5.44772 19 5 18.5523 5 18V10Z" color={"currentColor"} fill={"currentColor"} />
+    <path
+      d="M11 19H12C12.5523 19 13 18.5523 13 18V13.5C13 12 16 11 16 13V18.0004C16 18.5527 16.4477 19 17 19H18C18.5523 19 19 18.5523 19 18V12C19 10 17.5 9 15.5 9C13.5 9 13 10.5 13 10.5V10C13 9.44771 12.5523 9 12 9H11C10.4477 9 10 9.44772 10 10V18C10 18.5523 10.4477 19 11 19Z"
+      color={"currentColor"}
+      fill={"currentColor"}
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M20 1C21.6569 1 23 2.34315 23 4V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H20ZM20 3C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H20Z"
+      color={"currentColor"}
+      fill={"currentColor"}
+    />
+  </svg>
+);
+
+const GithubFilledIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width={props.width ?? 24} height={props.height ?? 24} viewBox="0 0 20 20" version="1.1" color={"currentColor"} fill={"currentColor"} xmlns="http://www.w3.org/2000/svg">
+    <title>github [#142]</title>
+    <desc>Created with Sketch.</desc>
+    <defs></defs>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" color={"currentColor"} fill={"currentColor"}>
+        <g id="icons" transform="translate(56.000000, 160.000000)">
+          <path
+            d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
+            id="github-[#142]"
+          ></path>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
 const GithubIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg width={props.width ?? 24} height={props.height ?? 24} viewBox="0 0 24 24" color={"currentColor"} fill={"currentColor"} xmlns="http://www.w3.org/2000/svg">
     <path
@@ -270,11 +307,20 @@ const MySQLIcon = (props: React.SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
+const LeetcodeIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width={props.width ?? 24} height={props.height ?? 24} color={"currentColor"} fill={"currentColor"} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+    <path d="M21.469 23.907l-3.595 3.473c-0.624 0.625-1.484 0.885-2.432 0.885s-1.807-0.26-2.432-0.885l-5.776-5.812c-0.62-0.625-0.937-1.537-0.937-2.485 0-0.952 0.317-1.812 0.937-2.432l5.76-5.844c0.62-0.619 1.5-0.859 2.448-0.859s1.808 0.26 2.432 0.885l3.595 3.473c0.687 0.688 1.823 0.663 2.536-0.052 0.708-0.713 0.735-1.848 0.047-2.536l-3.473-3.511c-0.901-0.891-2.032-1.505-3.261-1.787l3.287-3.333c0.688-0.687 0.667-1.823-0.047-2.536s-1.849-0.735-2.536-0.052l-13.469 13.469c-1.307 1.312-1.989 3.113-1.989 5.113 0 1.996 0.683 3.86 1.989 5.168l5.797 5.812c1.307 1.307 3.115 1.937 5.115 1.937 1.995 0 3.801-0.683 5.109-1.989l3.479-3.521c0.688-0.683 0.661-1.817-0.052-2.531s-1.849-0.74-2.531-0.052zM27.749 17.349h-13.531c-0.932 0-1.692 0.801-1.692 1.791 0 0.991 0.76 1.797 1.692 1.797h13.531c0.933 0 1.693-0.807 1.693-1.797 0-0.989-0.76-1.791-1.693-1.791z" />
+  </svg>
+);
+
 export {
   ESLintIcon,
   ExpressIcon,
+  GithubFilledIcon,
   GithubIcon,
   JavaIcon,
+  LeetcodeIcon,
+  LinkedInIcon,
   MongoDBIcon,
   MoonIcon,
   MySQLIcon,
